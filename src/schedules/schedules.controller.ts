b2595cbe67f2dff49f8311d0c48ccb9e379b6885
wafu-alt <rhfemtlfem@naver.com>
2024-09-04 04:46:23 +0900

    Update:[schedules.controller] 컨트롤 수동 업데이트 추가

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -3,4 +3,14 @@ import { SchedulesService } from './schedules.service';
 
 @Controller('schedules')
 export class SchedulesController {
+  private logger = new Logger('SchedulesController');
+  constructor(private schedulesService: SchedulesService) {}
+
+  @Get('/passivityRunToTask')
+  fetchAllItemSaveTasks() {
+    this.logger.log(
+      `수동으로 Task실행 : EFestivalList, EFestivalDetail 테이블에 contentid값에 대한 정보를 저장`,
+    );
+    return this.schedulesService.fetchAllItemSaveTasks();
+  }
 }
