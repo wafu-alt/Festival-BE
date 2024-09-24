e2137806903897111d089398dd99340d87a725ae
wafu-alt <rhfemtlfem@naver.com>
2024-09-24 16:40:20 +0900

    Core:[main.ts] cors 주소 출처 수정 및 추가

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,22 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // env에서 정보 불러오기
+  const configService = app.get(ConfigService);
+
+  // cors 허락 주소 파싱
+  const allowedOrigins = configService
+    .get<string>('ALLOWED_ORIGINS')
+    .split(',');
+
   const corsConfig = {
-    origin: ['http://localhost:3000'], //"*" 기본값
-    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE" , //기본값
+    origin: allowedOrigins, //"*" 기본값
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', //기본값
     // allowedHeaders: ['Content-Type', 'Authorization'] 어떤 헤더
     // credentials: true, // 쿠키, 인증 헤더 사용할 것인지?
     // exposedHeaders 사용할 헤더 추가
     // maxAge 캐싱할 시간 몇으로 설정?
-    // preflightContinue : false, // 응답 다음 미들웨어로 전달할 것인지?
+    preflightContinue: false, // 응답 다음 미들웨어로 전달할 것인지?
     optionsSuccessStatus: 200, // 응답 상태코드
   };
 
@@ -24,7 +32,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // 서버 포트 파싱
-  const configService = app.get(ConfigService);
   const serverPort = configService.get('SERVER_PORT');
 
   // 서버 실행
